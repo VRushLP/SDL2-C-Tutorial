6cdf5c55fef240e797645bba72a582d0c71423d3
Robert Ferguson <rmfarc@uw.edu>
2017-08-10 16:55:56 -0700

    08_geometry_rendering

--- SDL2-C/sdl_tutorial.c
+++ SDL2-C/sdl_tutorial.c
@@ -102,12 +102,6 @@ int loadMedia()
 	//Loading success flag
 	int success = 1;
 
-	gTexture = loadTexture("texture.png");
-	if(gTexture == NULL) {
-		printf("Failed to load texture image!\n");
-		success = 0;
-	}
-
 	return success;
 }
 
@@ -182,10 +176,45 @@ int main(int argc, char* args[])
 				}
 
 				//Clear screen
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 				SDL_RenderClear(gRenderer);
 
-				//Render texture to screen
-				SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+				//Render red filled quad
+				SDL_Rect fillRect = {
+				        SCREEN_WIDTH / 4,
+				        SCREEN_HEIGHT / 4,
+				        SCREEN_WIDTH / 2,
+				        SCREEN_HEIGHT / 2
+                                };
+
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+				SDL_RenderFillRect(gRenderer, &fillRect);
+
+				//Render green outlined quad
+				SDL_Rect outlineRect = {
+				        SCREEN_WIDTH / 6,
+				        SCREEN_HEIGHT / 6,
+				        SCREEN_WIDTH * 2 / 3,
+				        SCREEN_HEIGHT * 2 / 3
+                                };
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+				SDL_RenderDrawRect(gRenderer, &outlineRect);
+
+				//Draw blue horizontal line
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
+				SDL_RenderDrawLine(
+                                                gRenderer,
+                                                0, SCREEN_HEIGHT / 2,
+                                                SCREEN_WIDTH,
+                                                SCREEN_HEIGHT / 2
+                                );
+
+				//Draw vertical line of yellow dots
+				int i = 0;
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
+				for(; i < SCREEN_HEIGHT; i += 4) {
+					SDL_RenderDrawPoint( gRenderer, SCREEN_WIDTH / 2, i );
+				}
 
 				//Update screen
 				SDL_RenderPresent(gRenderer);
